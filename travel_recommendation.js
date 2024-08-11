be5529dd706ae4d6813200275cd822f1b31c942f
tzelinska <zelinska.t.p@gmail.com>
2024-08-11 15:21:58 -0400

    Final Project Update

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,69 +1,74 @@
 
-    const btnSearch = document.getElementById('btnSearch');
-    const btnClear = document.getElementById('btnClear');
-    
-    function searchDestination() {
-        const input = document.getElementById('destinationInput').value.toLowerCase();
-        const resultDiv = document.getElementById('result');
-        resultDiv.innerHTML = '';
-    
-        if (!input) {
-            resultDiv.innerHTML = 'Please enter a keyword to search.';
-            return;
-        }
-    
-        fetch('travel_recommendation_api.json')
-          .then(response => response.json())
-          .then(data => {
-            let found = false;
-            console.log(data);
+const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
 
-            if (input === 'beach' || input === 'beaches') {
-                found = true;
-                data.beaches.forEach(destination => {
-                    resultDiv.innerHTML += `<div>
-                                                <img src="${destination.imageUrl}" alt="${destination.name}" />
-                                                <h2>${destination.name}</h2>
-                                                <p>${destination.description}</p>
-                                            </div>`;
-                });
-            } else if (input === 'temple' || input === 'temples') {
-                found = true;
-                data.temples.forEach(destination => {
-                    resultDiv.innerHTML += `<div>
-                                                <img src="${destination.imageUrl}" alt="${destination.name}" />
-                                                <h2>${destination.name}</h2>
-                                                <p>${destination.description}</p>
-                                            </div>`;
-                });
-            } else {
-                data.countries.forEach(country => {
-                    country.cities.forEach(city => {
-                        if (city.name.toLowerCase().includes(input)) {
-                            found = true;
-                            resultDiv.innerHTML += `<div>
-                                                        <img src="${city.imageUrl}" alt="${city.name}" />
-                                                        <h2>${city.name}</h2>
-                                                        <p>${city.description}</p>
-                                                    </div>`;
-                        }
-                    });
-                });
-            }
-            resultDiv.innerHTML
-            if (!found) {
-                resultDiv.innerHTML = 'Destination not found.';
-            }
-          })
-          .catch(error => {
-            console.error('Error:', error);
-            resultDiv.innerHTML = 'An error occurred while fetching data.';
-          });
+function searchDestination() {
+    const input = document.getElementById('destinationInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    if (!input) {
+        resultDiv.innerHTML = 'Please enter a keyword to search.';
+        return;
     }
-    
-    btnSearch.addEventListener('click', searchDestination);
 
-    document.getElementById('btnClear').addEventListener('click', function() {
-        document.getElementById('result').innerHTML = '';
-    });
-    
+    fetch('travel_recommendation_api.json')
+      .then(response => response.json())
+      .then(data => {
+        let found = false;
+        console.log(data);
+        console.log(input)
+
+        if (input === 'beach' || input === 'beaches') {
+            found = true;
+            console.log("beach detected")
+
+            data.beaches.forEach(destination => {
+                resultDiv.innerHTML += `<div>
+                                            <img src="${destination.imageUrl}" alt="${destination.name}" />
+                                            <h2>${destination.name}</h2>
+                                            <p>${destination.description}</p>
+                                        </div>`;
+            });
+        } else if (input === 'temple' || input === 'temples') {
+            found = true;
+            console.log("temple detected")
+
+            data.temples.forEach(destination => {
+                resultDiv.innerHTML += `<div>
+                                            <img src="${destination.imageUrl}" alt="${destination.name}" />
+                                            <h2>${destination.name}</h2>
+                                            <p>${destination.description}</p>
+                                        </div>`;
+            });
+        } else if (input === 'country' || input === 'countries') {
+            found = true;
+            console.log("country detected")
+            data.countries.forEach(country => {
+                    country.forEach(destination => {
+                        resultDiv.innerHTML += `<div>
+                                                    <img src="${destination.imageUrl}" alt="${destination.name}" />
+                                                    <h2>${destination.name}</h2>
+                                                    <p>${destination.description}</p>
+                                                </div>`;
+
+                    })
+                        
+                });
+        }
+        if (!found) {
+            resultDiv.innerHTML = 'Destination not found.';
+        }
+
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+}
+
+btnSearch.addEventListener('click', searchDestination);
+
+document.getElementById('btnClear').addEventListener('click', function() {
+    document.getElementById('result').innerHTML = '';
+});
